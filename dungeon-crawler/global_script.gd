fd720beabe858acacd390c9edfe47d2d4279a44c
simco-gabriela <gabrielasimco@yahoo.com>
2025-03-31 23:24:59 +0300

    Change inventory to use item-count tuples

--- dungeon-crawler/global_script.gd
+++ dungeon-crawler/global_script.gd
@@ -5,11 +5,21 @@ var max_hp : int = 100
 var inventory : Array = []
 var equipped_weapon : String = ""
 
-func add_to_inventory(item: String):
-	inventory.append(item)
+func add_to_inventory(item_id: String):
+	for i in inventory.size():
+		if inventory[i][0] == item_id:
+			inventory[i] = [item_id, inventory[i][1] + 1]
+			return
+	inventory.append([item_id, 1])
 
-func remove_from_inventory(item: String):
-	inventory.erase(item)
+func remove_from_inventory(item_id: String):
+	for i in inventory.size():
+		if inventory[i][0] == item_id:
+			if inventory[i][1] > 1:
+				inventory[i] = [item_id, inventory[i][1] - 1]
+			else:
+				inventory.remove_at(i)
+			return
 
 func equip_weapon(weapon: String):
 	equipped_weapon = weapon
